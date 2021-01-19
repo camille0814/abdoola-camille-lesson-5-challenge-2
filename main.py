405c08859cb694bd8f47b5d51917437cb581d11e
camille0814 <72763933+camille0814@users.noreply.github.com>
2021-01-19 09:47:48 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+while True:
+    music.set_volume(40) 
+    if input.light_level() < 6:
+        music.wawawawaa.play_until_done()
+    else:
+        music.pew_pew.play_until_done()
---
+++ main.ts
@@ -0,0 +1,9 @@
+while (true) {
+    music.setVolume(40)
+    if (input.lightLevel() < 6) {
+        music.wawawawaa.playUntilDone()
+    } else {
+        music.pewPew.playUntilDone()
+    }
+    
+}
